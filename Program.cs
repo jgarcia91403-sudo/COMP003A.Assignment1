f35d07fec7129040c91ea1fe6bd17b9a5fa4b8f5
Jgarc <Jgarc@DESKTOP-KRTL31P>
2026-01-16 15:36:22 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace COMP003A.Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Favorit,color blue!");
+            Console.WriteLine("play sports!");
+            Console.WriteLine("love to go camping!");
+            Console.WriteLine("play video games!");
+            Console.WriteLine("going to the beach with friends!");
+            Console.WriteLine("My favorite baseball team is the giants!");
+            Console.WriteLine("My favorite sport to play is basketball!");
+            Console.WriteLine("Playing poker with my family!");
+            Console.WriteLine("My favorite video game to play is madden that is football!");
+            Console.WriteLine("My favorite tv show is supernatrual!");
+        }
+    }
+}
