0b3c2988ea0b68621ff8adf0c2f7349a5deb029d
Jgarc <Jgarc@DESKTOP-KRTL31P>
2026-01-16 18:08:27 -0800

    I added lines 28-37

--- Program.cs
+++ Program.cs
@@ -25,6 +25,17 @@
             Console.WriteLine("The sport i played the best in is baseball!");
             Console.WriteLine("Im a quick learner!");
             Console.WriteLine("I have my driver license!");
+            Console.WriteLine("I pay for my own car insurance!");
+            Console.WriteLine("I enloy doing homework!");
+            Console.WriteLine("I like going to the snow during winter!");
+            Console.WriteLine("I've went to 2 other states in my lifetime!");
+            Console.WriteLine("Im not a very quick at reading!");
+            Console.WriteLine("I collect pokemon cards with my friends!");
+            Console.WriteLine("I have a garden!");
+            Console.WriteLine("I graduted high school in 2022!");
+            Console.WriteLine("I enjoy working on cars during my free time!");
+            Console.WriteLine("I been deep sea fishing 2 times!");
+
         }
     }
 }
